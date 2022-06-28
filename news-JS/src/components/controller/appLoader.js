2f5249ed2f4c8ef972ca1aaf0d323c12c7f5e245
Katherina Kaliaha <mellinda077@mail.ru>
2022-06-28 12:42:12 +0300

    feat: add key for task, change path to folder dist

--- news-JS/src/components/controller/appLoader.js
+++ news-JS/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '09a659ea28d346a4b74415ac6bd1e634', // получите свой ключ https://newsapi.org/
         });
     }
 }
