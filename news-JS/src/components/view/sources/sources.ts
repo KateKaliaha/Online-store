c7015d0a36d47b08cb57c672ea608025f77b604e
Katherina Kaliaha <mellinda077@mail.ru>
2022-06-30 11:52:29 +0300

    refactor: change extensions from js to ts in news, sources files, add types variables and methods

---
+++ news-JS/src/components/view/sources/sources.ts
@@ -0,0 +1,32 @@
+import './sources.css';
+
+interface source {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+class Sources {
+    draw(data: source[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+
+        data.forEach((item: source) => {
+            const sourceClone = (sourceItemTemp as HTMLTemplateElement).content.cloneNode(true) as HTMLElement;
+
+            (sourceClone.querySelector('.source__item-name') as HTMLTemplateElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLTemplateElement).setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+            console.log(sourceClone);
+        });
+
+        (document.querySelector('.sources') as HTMLTemplateElement).append(fragment);
+    }
+}
+
+export default Sources;
