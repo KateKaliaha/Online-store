ac8e39dc57694c90e5d3afaabcb17f810db15e71
Katherina Kaliaha <mellinda077@mail.ru>
2022-07-03 17:31:15 +0300

    fix: add sort by category of news, add come out list, add styles for list, change styles for footer

--- news-JS/src/components/view/sources/sources.ts
+++ news-JS/src/components/view/sources/sources.ts
@@ -5,18 +5,35 @@ class Sources {
     public draw(data: Readonly<source[]>): void {
         const fragment = document.createDocumentFragment();
         const sourceItemTemp = document.querySelector('#sourceItemTemp');
-
+        const fragment1 = document.createDocumentFragment();
+        const fragment2 = document.createDocumentFragment();
+        const fragment3 = document.createDocumentFragment();
+        const fragment4 = document.createDocumentFragment();
+        const fragment5 = document.createDocumentFragment();
         data.forEach((item: source) => {
             const sourceClone = (sourceItemTemp as HTMLTemplateElement).content.cloneNode(true) as HTMLElement;
-
             (sourceClone.querySelector('.source__item-name') as HTMLTemplateElement).textContent = item.name;
             (sourceClone.querySelector('.source__item') as HTMLTemplateElement).setAttribute('data-source-id', item.id);
-
-            fragment.append(sourceClone);
-            console.log(sourceClone);
+            if (item.category === 'general') {
+                fragment.append(sourceClone);
+            } else if (item.category === 'business') {
+                fragment1.append(sourceClone);
+            } else if (item.category === 'technology') {
+                fragment2.append(sourceClone);
+            } else if (item.category === 'sports') {
+                fragment3.append(sourceClone);
+            } else if (item.category === 'entertainment') {
+                fragment4.append(sourceClone);
+            } else {
+                fragment5.append(sourceClone);
+            }
         });
-
-        (document.querySelector('.sources') as HTMLTemplateElement).append(fragment);
+        (document.querySelector('.general') as HTMLTemplateElement).append(fragment);
+        (document.querySelector('.business') as HTMLTemplateElement).append(fragment1);
+        (document.querySelector('.technology') as HTMLTemplateElement).append(fragment2);
+        (document.querySelector('.sports') as HTMLTemplateElement).append(fragment3);
+        (document.querySelector('.entertainment') as HTMLTemplateElement).append(fragment4);
+        (document.querySelector('.health-science') as HTMLTemplateElement).append(fragment5);
     }
 }
 
