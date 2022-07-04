5055cdc66aee1833f0b01515fccf238170d6936b
Katherina Kaliaha <mellinda077@mail.ru>
2022-07-04 10:48:59 +0300

    refactor: change link by news files

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '08ab426d825742b293c8231e24d0a958', // получите свой ключ https://newsapi.org/
         });
     }
