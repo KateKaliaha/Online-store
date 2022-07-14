a150b160e7f056b6903a8d3b5c7dc6ed12f58653
Katherina Kaliaha <mellinda077@mail.ru>
2022-07-14 20:42:04 +0300

    refactor: change function sort by categoties, remove unusial code, add spaces to css files

--- online-store/src/app/sort.ts
+++ online-store/src/app/sort.ts
@@ -1,69 +1,48 @@
 const select = document.querySelector('.sort-list') as HTMLSelectElement;
-select.addEventListener('change', function(){
+
+select.addEventListener('change', function() {
   if (this.value === 'price-up') {
-    sortUpNumber('data-price');
+    sort('data-price','price-up');
   }
   if(this.value === 'price-down') {
-    sortDownNumber('data-price');
+    sort('data-price', 'price-down');
   }
   if(this.value === 'alphabet-up') {
-    sortUpLetter('data-name');
+    sort('data-name', 'alphabet-up');
   }
   if(this.value === 'alphabet-down') {
-    sortDownLetter('data-name');
+    sort('data-name', 'alphabet-down');
   }
 });
 
-function sortUpNumber(sortType: string) {
+function sort(sortType: string, valueSelect:string) {
   const content = document.querySelector('.content') as Element;
   for (let i = 0; i < content?.children.length; i++) {
     for (let j = i; j < content?.children.length; j++) {
       if(content.children[i].getAttribute(sortType) !== null) {
-        if(+(content.children[i].getAttribute(sortType) as string) > +(content.children[j].getAttribute(sortType) as string)) {
-        const replacedNode = content.replaceChild(content.children[j], content.children[i]);
-        insertAfter(replacedNode, content.children[i]);
-        }
-      }
-    }
-  }
-}
-
-function sortUpLetter(sortType: string) {
-  const content = document.querySelector('.content') as Element;
-  for (let i = 0; i < content?.children.length; i++) {
-    for (let j = i; j < content?.children.length; j++) {
-      if(content.children[i].getAttribute(sortType) !== null) {
-        if((content.children[i].getAttribute(sortType) as string) > (content.children[j].getAttribute(sortType) as string)) {
-        const replacedNode = content.replaceChild(content.children[j], content.children[i]);
-        insertAfter(replacedNode, content.children[i]);
-        }
-      }
-    }
-  }
-}
-
-function sortDownNumber(sortType: string) {
-  const content = document.querySelector('.content') as Element;
-  for (let i = 0; i < content?.children.length; i++) {
-    for (let j = i; j < content?.children.length; j++) {
-      if(content.children[i].getAttribute(sortType) !== null) {
-        if(+(content.children[i].getAttribute(sortType) as string) < +(content.children[j].getAttribute(sortType) as string)) {
+        if (valueSelect === 'price-up') {
+          if(+(content.children[i].getAttribute(sortType) as string) > +(content.children[j].getAttribute(sortType) as string)) {
           const replacedNode = content.replaceChild(content.children[j], content.children[i]);
           insertAfter(replacedNode, content.children[i]);
+          }
         }
-      }
-    }
-  }
-}
-
-function sortDownLetter(sortType: string) {
-  const content = document.querySelector('.content') as Element;
-  for (let i = 0; i < content?.children.length; i++) {
-    for (let j = i; j < content?.children.length; j++) {
-      if(content.children[i].getAttribute(sortType) !== null) {
-        if((content.children[i].getAttribute(sortType) as string) < (content.children[j].getAttribute(sortType) as string)) {
-          const replacedNode = content.replaceChild(content.children[j], content.children[i]);
-          insertAfter(replacedNode, content.children[i]);
+        if (valueSelect === 'alphabet-up') {
+          if((content.children[i].getAttribute(sortType) as string) > (content.children[j].getAttribute(sortType) as string)) {
+            const replacedNode = content.replaceChild(content.children[j], content.children[i]);
+            insertAfter(replacedNode, content.children[i]);
+          }
+        }
+        if (valueSelect === 'price-down') {
+          if(+(content.children[i].getAttribute(sortType) as string) < +(content.children[j].getAttribute(sortType) as string)) {
+            const replacedNode = content.replaceChild(content.children[j], content.children[i]);
+            insertAfter(replacedNode, content.children[i]);
+          }
+        }
+        if (valueSelect === 'alphabet-down') {
+          if((content.children[i].getAttribute(sortType) as string) < (content.children[j].getAttribute(sortType) as string)) {
+            const replacedNode = content.replaceChild(content.children[j], content.children[i]);
+            insertAfter(replacedNode, content.children[i]);
+          }
         }
       }
     }
